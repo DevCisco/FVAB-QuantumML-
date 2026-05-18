1111ab428d359a169e13e4e0820e96b8a6a48285
Francesco Pisaturo <97796423+DevCisco@users.noreply.github.com>
2026-05-18 12:51:40 +0200

    Class update in import section
    Due to some updates in IBM's library Qiskit, the class "qiskit.circuit.library.n_local.real_amplitudes.RealAmplitudes" has been deprecated and it will be removed in version 3.0 of the library. The commit aims at guaranteeing the capability of the code to be used with the newer version of Qiskit.

--- vqc_fewshot_engine.py
+++ vqc_fewshot_engine.py
@@ -1,14 +1,11 @@
 import torch
 import torch.nn as nn
 from qiskit.circuit import QuantumCircuit, ParameterVector
-from qiskit.circuit.library import RealAmplitudes
+from qiskit.circuit.library import real_amplitudes
 from qiskit_machine_learning.connectors import TorchConnector
 from qiskit_machine_learning.neural_networks import EstimatorQNN
 from qiskit.quantum_info import SparsePauliOp
 
-# FIX BUG 5: il resto del progetto usa EstimatorV2 (già corretto in hybrid_engine
-# e stabilized_vqc_model). Usare la versione legacy 'Estimator' introduce
-# inconsistenza nei risultati di simulazione tra moduli.
 from qiskit_aer.primitives import EstimatorV2 as Estimator
 
 
@@ -21,12 +18,7 @@ class FewShotVQC(nn.Module):
         x = ParameterVector('x', d_latent)
 
         num_cycles = (d_latent + n_qubits - 1) // n_qubits
-
-        # FIX BUG 3: nella versione originale 'ansatz = RealAmplitudes(...)' veniva
-        # ricreato ad ogni ciclo con nomi di parametri identici. Qiskit deduplica i
-        # parametri per nome: tutti i layer variazionali condividevano gli stessi
-        # pesi → gradi di libertà ridotti e gradiente sbagliato.
-        # Ogni ciclo ha ora il proprio ParameterVector distinto 'theta_l{l}'.
+        
         weight_params_all = []
         feat_idx = 0
         for l in range(num_cycles):
@@ -35,17 +27,13 @@ class FewShotVQC(nn.Module):
                     qc.ry(x[feat_idx], i)
                     feat_idx += 1
 
-            ansatz_template = RealAmplitudes(n_qubits, reps=1, entanglement='linear')
+            ansatz_template = real_amplitudes(n_qubits, reps=1, entanglement='linear')
             n_w = ansatz_template.num_parameters
             w = ParameterVector(f'theta_l{l}', n_w)
             weight_params_all.extend(list(w))
             param_map = {ansatz_template.parameters[i]: w[i] for i in range(n_w)}
             qc.compose(ansatz_template.assign_parameters(param_map), inplace=True)
 
-        # FIX BUG 2: 'qc.parameters[d_latent:]' opera su un ParameterView (set
-        # non ordinato): l'ordine è arbitrario e non deterministico tra esecuzioni.
-        # Separiamo esplicitamente input params (prefisso 'x[') da weight params
-        # (prefisso 'theta_l') e ordiniamo ciascun gruppo numericamente.
         all_params = qc.parameters
         input_params_sorted = sorted(
             [p for p in all_params if p.name.startswith('x[')],
@@ -69,9 +57,6 @@ class FewShotVQC(nn.Module):
         )
         self.qnn_layer = TorchConnector(qnn)
 
-        # FIX BUG 4: nn.Linear(1, 4) causava RuntimeError al primo forward.
-        # EstimatorQNN con n_qubits=4 osservabili produce output (batch, 4),
-        # non (batch, 1). Il layer deve essere Linear(n_qubits, 4).
         self.head = nn.Linear(n_qubits, 4)
 
     def forward(self, x):
